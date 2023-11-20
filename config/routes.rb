e6042eca4666e048b6e00b4ca1778963a12dcff6
Jennifer Crétal <jennifer.cretal@gmail.com>
2023-11-20 15:19:44 +0100

    Dashboard structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'games/index'
-  get 'games/show'
-  get 'games/new'
-  get 'games/create'
-  get 'games/edit'
-  get 'games/update'
-  get 'games/destroy'
+  # get 'games/index'
+  # get 'games/show'
+  # get 'games/new'
+  # get 'games/create'
+  # get 'games/edit'
+  # get 'games/update'
+  # get 'games/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
