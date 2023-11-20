c98e89d079eec5f48b252a303b050d2c457387b1
Jennifer Crétal <jennifer.cretal@gmail.com>
2023-11-20 12:30:06 +0100

    Add Game model

---
+++ db/migrate/20231120112836_create_games.rb
@@ -0,0 +1,14 @@
+class CreateGames < ActiveRecord::Migration[7.1]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.integer :number_of_players_min
+      t.integer :number_of_players_max
+      t.string :type
+      t.float :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
