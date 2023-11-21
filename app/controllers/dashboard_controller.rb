423a264eba10023889c9fbcf7c269cc487e20577
Nicolas Cureau <nicolascureau@gmail.com>
2023-11-21 17:56:20 +0100

    modify login with city

--- app/controllers/dashboard_controller.rb
+++
@@ -1,6 +0,0 @@
-class DashboardController < ApplicationController
-  def index
-    @games = Game.where(user_id: current_user)
-    @bookings = Booking.where(user_id: current_user)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_095951) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_155800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_095951) do
     t.string "address"
     t.string "first_name"
     t.string "last_name"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -88,4 +89,3 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_095951) do
   add_foreign_key "bookings", "users"
   add_foreign_key "games", "users"
 end
-
