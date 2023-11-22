077cf9fefc7c5db55df59068608c4f08cf525785
Nicolas Cureau <nicolascureau@gmail.com>
2023-11-22 15:52:14 +0100

    modify-bug

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,5 +6,5 @@ class Game < ApplicationRecord
   validates :number_of_players_min, inclusion: { in: 1..6 }
   validates :number_of_players_max, inclusion: { in: 1..16 }
   validates :category, inclusion: ["Card Game", "Adventure", "Farming", "Music", "Party Game", "Science Fiction", "Negotiation", "Children's Game", "Action/Dexterity", "Bluffing", "Murder/Mystery"]
-  validates :price, numericality: { greater_than_or_equal_to: 0.5 }
+  validates :price, numericality: { greater_than_or_equal_to: 1 }
 end
