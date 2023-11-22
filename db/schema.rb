e328c2b7024b889f9cffe13e1de5b3cb80e22eb0
Nicolas Cureau <nicolascureau@gmail.com>
2023-11-22 12:05:13 +0100

    adding seeds with image

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_160700) do
-
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_093401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_160700) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.string "image"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
