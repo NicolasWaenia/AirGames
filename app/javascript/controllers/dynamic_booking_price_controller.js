fd57ac9080832257e5e18df2a2c8dca0f1d8fee0
Jennifer Crétal <jennifer.cretal@gmail.com>
2023-11-23 18:12:22 +0100

    Fix errors JS dates

---
+++ app/javascript/controllers/dynamic_booking_price_controller.js
@@ -0,0 +1,50 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dynamic-booking-price"
+export default class extends Controller {
+
+  static targets = [ 'startTime', 'endTime', 'priceHolder' ];
+  static values = { price: Number };
+
+  connect() {
+    console.log("Hello");
+    this.element.addEventListener('change', (event) => {
+      console.log("Événement de changement détecté :", event);
+    });
+  }
+
+  updatePrice() {
+    console.log("Hello bis");
+    const startDate = this.startTimeTarget.value;
+    const endDate = this.endTimeTarget.value;
+
+    const days = this.diffInDays(startDate, endDate);
+
+    const totalValue = this.totalValue(days);
+
+    console.log(startDate);
+    console.log(endDate);
+    console.log(days);
+    console.log(totalValue);
+
+    this.setPriceValue(totalValue)
+  }
+
+  setPriceValue(value) {
+    this.priceHolderTarget.innerText = `Total price : ${value}€`;
+  }
+
+  diffInDays(startDate, endDate) {
+    if (startDate && endDate) {
+      const diffTime = (Date.parse(endDate) - Date.parse(startDate))
+      var days = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    } else {
+      var days = 0
+    }
+    return days + 1
+  }
+
+  totalValue(days) {
+    return (days) * this.priceValue
+  }
+}
