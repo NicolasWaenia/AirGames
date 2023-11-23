775319423ae799b5381b221b853176974bf94008
Marion Guedon <guedonmarion@live.fr>
2023-11-23 10:02:32 +0100

    reviews mngt

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :games
   has_many :bookings
+  has_many :reviews
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
